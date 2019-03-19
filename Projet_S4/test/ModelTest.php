cd2fca797ae3d90aadc610c371649a081d4f8923
Julian Bourdes <julian.bourdes@gmail.com>
2019-03-19 18:17:22 +0100

    ModelTest fini

--- Projet_S4/test/ModelTest.php
+++ Projet_S4/test/ModelTest.php
@@ -12,6 +12,17 @@ class ModelTest extends TestCase
      * @var Model
      */
     private $model;
+    
+    /**
+     * Tests Model::Init()
+     */
+    public function testInit()
+    {
+        // TODO Auto-generated ModelTest::testInit()
+        $this->markTestIncomplete("Init test not implemented");
+        
+        assertTrue(Model::Init());
+    }
 
     /**
      * Tests Model::getPDO()
@@ -20,86 +31,76 @@ class ModelTest extends TestCase
     {
         // TODO Auto-generated ModelTest::testGetPDO()
         $this->markTestIncomplete("getPDO test not implemented");
-
+        $pdo = new PDO("mysql:host=webinfo.iutmontp.univ-montp2.fr;dbname=bourdesj", "bourdesj", "Unicorn", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
         Model::Init();
-        Model::getPDO();
+        $this->assertEquals($pdo,Model::getPDO());
     }
 
-    /**
-     * Tests Model::Init()
-     */
-    public function testInit()
-    {
-        // TODO Auto-generated ModelTest::testInit()
-        $this->markTestIncomplete("Init test not implemented");
-
-        assertTrue(Model::Init());
-    }
-
-    /**
+    /* On ne peut pas tester les fonctions du modèle générique
+    
      * Tests Model::selectAll()
-     */
+     
     public function testSelectAll()
     {
         // TODO Auto-generated ModelTest::testSelectAll()
         $this->markTestIncomplete("selectAll test not implemented");
 
-        Model::selectAll(/* parameters */);
+        Model::selectAll();
     }
 
-    /**
+    
      * Tests Model::select()
-     */
+     
     public function testSelect()
     {
         // TODO Auto-generated ModelTest::testSelect()
         $this->markTestIncomplete("select test not implemented");
 
-        Model::select(/* parameters */);
+        Model::select(/* parameters );
     }
 
-    /**
-     * Tests Model::delete()
-     */
+    
+     Tests Model::delete()
+     
     public function testDelete()
     {
         // TODO Auto-generated ModelTest::testDelete()
         $this->markTestIncomplete("delete test not implemented");
 
-        Model::delete(/* parameters */);
+        Model::delete(/* parameters );
     }
 
     /**
      * Tests Model::update()
-     */
+     
     public function testUpdate()
     {
         // TODO Auto-generated ModelTest::testUpdate()
         $this->markTestIncomplete("update test not implemented");
 
-        Model::update(/* parameters */);
+        Model::update(/* parameters );
     }
 
     /**
      * Tests Model::created()
-     */
+     
     public function testCreated()
     {
         // TODO Auto-generated ModelTest::testCreated()
         $this->markTestIncomplete("created test not implemented");
 
-        Model::created(/* parameters */);
+        Model::created(/* parameters );
     }
 
     /**
      * Tests Model::selectAllWithArgs()
-     */
+     
     public function testSelectAllWithArgs()
     {
         // TODO Auto-generated ModelTest::testSelectAllWithArgs()
         $this->markTestIncomplete("selectAllWithArgs test not implemented");
 
-        Model::selectAllWithArgs(/* parameters */);
-    }
+        Model::selectAllWithArgs(/* parameters );
+    } */
 }
 
